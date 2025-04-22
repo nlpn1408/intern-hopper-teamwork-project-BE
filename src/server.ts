a1762a1a44a51ba1e2b35c02a693b106a3491814
nlpn1408 <nlpn1481999@gmail.com>
2025-04-22 23:20:59 +0700

    init project

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app: express.Application = express();
+const port: number = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
