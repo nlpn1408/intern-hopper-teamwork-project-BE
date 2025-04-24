77f705cede243e6d88196d913f0842e2022d7720
Nguyen Ngoc My <nguyenngocmy1311@gmail.com>
2025-04-24 20:58:36 +0700

    Revert "Merge branch 'BE07'"
    This reverts commit f32b59c2fc02ba1f51d7da7a81fabdb2a4d52e5a, reversing
    changes made to 8e988f5901408a0986a4477000aeebc3a41b5897.

--- src/routes/user.routes.ts
+++
@@ -1,5 +0,0 @@
-import Router from 'express';
-const router = Router();
-import UserController from '../controllers/user.controller';
-router.post('/register', UserController.createUser); 
-export default router;
